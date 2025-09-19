883c8ca4c2641ebbfec2c201aaf8f6c4ae5739f9
DevepMau <oshiettecs@gmail.com>
2025-09-19 20:40:49 -0300

    Se quito el marco a MainApp y se creo uno personalizado: -Se quito el marco de windows. -Se añadio una barra superior con un boton de cerra y maximizar. -La barra superior permite mover la app. -Se modifico el estilo css.

--- elSantiGestion/src/com/elsantigestion/MainApp.java
+++ elSantiGestion/src/com/elsantigestion/MainApp.java
@@ -4,19 +4,30 @@ import com.elsantigestion.dao.DatabaseSetup;
 import com.elsantigestion.ui.ClienteView;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class MainApp extends Application {
+	
+	private double xOffset = 0;
+    private double yOffset = 0;
 
     @Override
     public void start(Stage stage) {
         DatabaseSetup.init();
+        stage.initStyle(StageStyle.UNDECORATED);
 
         // Logo
         Image logo = new Image(getClass().getResource("/logo/logo.png").toExternalForm());
@@ -33,6 +44,40 @@ public class MainApp extends Application {
 
         Button[] botones = {btnClientes, btnPedidos, btnGastos, btnCronograma, btnReportes};
 
+        // Barra superior
+        Button btnCerrar = new Button("X");
+        btnCerrar.setOnAction(e -> stage.close());
+        
+        Button btnMaximizar = new Button("⬜");
+        btnMaximizar.setOnAction(e -> stage.setMaximized(!stage.isMaximized()));
+        
+        HBox botonesBox = new HBox();
+        botonesBox.getChildren().addAll(btnMaximizar, btnCerrar);
+        
+        Label nombreApp = new Label("El Santi Gestion v1");
+        
+        Region spacer = new Region();
+        
+        HBox barraSuperior = new HBox();
+        barraSuperior.setAlignment(Pos.TOP_RIGHT);
+        barraSuperior.setSpacing(10);
+        barraSuperior.getChildren().addAll(nombreApp, spacer, botonesBox);
+	    HBox.setMargin(nombreApp, new Insets(15, 10, 0, 10));
+	    HBox.setHgrow(spacer, Priority.ALWAYS);
+	    barraSuperior.getStyleClass().add("barra-superior");
+	    
+	    barraSuperior.setOnMousePressed(event -> {
+	        // Guardamos la posición del mouse cuando se hace clic
+	        xOffset = event.getSceneX();
+	        yOffset = event.getSceneY();
+	    });
+	    
+	    barraSuperior.setOnMouseDragged(event -> {
+	        // Movemos la ventana según la posición del mouse
+	        barraSuperior.getScene().getWindow().setX(event.getScreenX() - xOffset);
+	        barraSuperior.getScene().getWindow().setY(event.getScreenY() - yOffset);
+	    });
+        
         // Clase común para todos los botones
         for (Button btn : botones) {
             btn.getStyleClass().add("menu-boton");
@@ -43,7 +88,9 @@ public class MainApp extends Application {
 
         // Panel principal
         BorderPane root = new BorderPane();
+        root.setTop(barraSuperior);
         root.setLeft(menuLateral);
+        root.getStyleClass().add("panel");
 
         // Función para manejar selección de botón
         for (Button btn : botones) {
