7537dd53b6a5560522ad9976baa63fd07a11d09d
DevepMau <oshiettecs@gmail.com>
2025-09-21 13:23:48 -0300

    Se dio formato a la tabla de ClienteView: -Se creo tabla.css que contiene el formato de todas las futuras tablas. -Modificaciones menores en ClienteView.

--- elSantiGestion/src/com/elsantigestion/ui/ClienteView.java
+++ elSantiGestion/src/com/elsantigestion/ui/ClienteView.java
@@ -3,6 +3,7 @@ package com.elsantigestion.ui;
 import com.elsantigestion.dao.ClienteDAO;
 import com.elsantigestion.model.Cliente;
 
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.geometry.Insets;
@@ -16,6 +17,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.util.Callback;
 
@@ -24,7 +26,7 @@ public class ClienteView extends VBox {
 	
 	private TableView<Cliente> tabla;
     private ClienteDAO dao;
-    private final int iconoTamaño = 15;
+    private final int iconoTamaño = 20;
 
     @SuppressWarnings("unchecked")
     public ClienteView() {
@@ -44,27 +46,57 @@ public class ClienteView extends VBox {
     	
         // Tabla de clientes
         tabla = new TableView<>();
-        tabla.getStyleClass().add("table-view");
+        //tabla.getStyleClass().add("table-view");
+        tabla.setStyle("-fx-background-color: transparent;");
 
         TableColumn<Cliente, String> colNombre = new TableColumn<>("Nombre");
         colNombre.setCellValueFactory(c -> new SimpleStringProperty(c.getValue().getNombre()));
+        colNombre.setId("col-nombre");
+        colNombre.getStyleClass().add("columnaPersonalizada");
 
         TableColumn<Cliente, String> colTelefono = new TableColumn<>("Teléfono");
         colTelefono.setCellValueFactory(c -> new SimpleStringProperty(c.getValue().getTelefono()));
+        colTelefono.getStyleClass().add("columnaPersonalizada");
         
         TableColumn<Cliente, String> colLocalidad = new TableColumn<>("Localidad");
         colLocalidad.setCellValueFactory(c -> new SimpleStringProperty(c.getValue().getLocalidad()));
-
+        colLocalidad.getStyleClass().add("columnaPersonalizada");
+        
         TableColumn<Cliente, String> colDireccion = new TableColumn<>("Direccion");
         colDireccion.setCellValueFactory(c -> new SimpleStringProperty(c.getValue().getDireccion()));
+        colDireccion.getStyleClass().add("columnaPersonalizada");
         
         TableColumn<Cliente, String> colEmail = new TableColumn<>("Email");
         colEmail.setCellValueFactory(c -> new SimpleStringProperty(c.getValue().getEmail()));
+        colEmail.getStyleClass().add("columnaPersonalizada");
         
         TableColumn<Cliente, Boolean> colActivo = new TableColumn<>("Activo");
-        colActivo.setCellValueFactory(c -> new SimpleBooleanProperty(c.getValue().isActivo()));
+        colActivo.setCellValueFactory(c -> new ReadOnlyObjectWrapper<>(c.getValue().isActivo()));
+        colActivo.getStyleClass().add("columnaPersonalizada");
 
-        TableColumn<Cliente, Void> colAcciones = new TableColumn<>("Acciones");
+        // CellFactory para mostrar "Activo"/"Inactivo" con fondo verde/rojo
+        colActivo.setCellFactory(col -> new TableCell<Cliente, Boolean>() {
+            @Override
+            protected void updateItem(Boolean item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setText(null);
+                    setStyle("");
+                } else {
+                    if (item) {
+                        setText("Activo");
+                        setStyle("-fx-text-fill: green; -fx-padding: 0 0 -1 0;");
+                    } else {
+                        setText("Inactivo");
+                        setStyle("-fx-text-fill: red; -fx-padding: 0 0 -1 0;");
+                    }
+                }
+                setAlignment(Pos.CENTER);
+            }
+        });
+        
+        TableColumn<Cliente, Void> colAcciones = new TableColumn<>("");
+        colAcciones.getStyleClass().add("columnaPersonalizada");
         Callback<TableColumn<Cliente, Void>, TableCell<Cliente, Void>> cellFactory = param -> {
             return new TableCell<>() {
             	Image borrar = new Image(getClass().getResource("/iconos/borrar.png").toExternalForm());
@@ -105,6 +137,7 @@ public class ClienteView extends VBox {
                 	btnEliminar.getStyleClass().add("table-boton");
                 	iconoBorrar.setFitWidth(iconoTamaño);
                 	iconoBorrar.setFitHeight(iconoTamaño);
+                	setStyle("-fx-padding: 0 0 0 0;");
                 }
 
                 @Override
@@ -114,7 +147,8 @@ public class ClienteView extends VBox {
                         setGraphic(null);
                     } else {
                         HBox contenedor = new HBox(5, btnEditar, btnEliminar);
-                        contenedor.setPadding(new Insets(2, 0, 2, 0));
+                        contenedor.setPadding(new Insets(0, 0, 0, 0));
+                        contenedor.setAlignment(Pos.CENTER);
                         setGraphic(contenedor);
                     }
                 }
@@ -125,7 +159,7 @@ public class ClienteView extends VBox {
 
         tabla.getColumns().addAll(colNombre, colTelefono, colLocalidad, colDireccion, colEmail, colActivo);
         
-        tabla.getColumns().add(colAcciones);
+        tabla.getColumns().add(colAcciones);  
 
         // Cargar datos iniciales
         refrescarTabla();
@@ -139,12 +173,42 @@ public class ClienteView extends VBox {
         // Layout
         this.getChildren().addAll(barraAcciones, tabla);
         
+        tabla.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        VBox.setVgrow(tabla, Priority.ALWAYS);
+        tabla.setPrefWidth(Double.MAX_VALUE);
+
+        // Columnas con mínimo ancho
+        colNombre.setMinWidth(150);
+        colTelefono.setMinWidth(100);
+        colLocalidad.setMinWidth(100);
+        colDireccion.setMinWidth(150);
+        colEmail.setMinWidth(120);
+        colActivo.setMinWidth(80);
+        colAcciones.setMinWidth(80);
+        //centrarColumna(colActivo);  
+
+        
+        this.getStylesheets().add(
+    	        getClass().getResource("/com/elsantigestion/css/tabla.css").toExternalForm()
+    	    );
+        
     }
         
     private void refrescarTabla() {
     	
     	tabla.getItems().setAll(dao.obtenerClientes());
     	
+    }
+    
+    private <T> void centrarColumna(TableColumn<Cliente, T> columna) {
+        columna.setCellFactory(col -> new TableCell<Cliente, T>() {
+            @Override
+            protected void updateItem(T item, boolean empty) {
+                super.updateItem(item, empty);
+                setText(empty || item == null ? null : item.toString());
+                setAlignment(Pos.CENTER);
+            }
+        });
     }
         
 }
