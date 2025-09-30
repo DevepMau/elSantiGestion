bb62518261080530bc4747e577c427800c63c5d2
DevepMau <oshiettecs@gmail.com>
2025-09-29 22:06:44 -0300

    Se creo ServicioEventualForm y se modifico formulario.css

---
+++ elSantiGestion/src/com/elsantigestion/ui/ServicioEventualForm.java
@@ -0,0 +1,265 @@
+package com.elsantigestion.ui;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.elsantigestion.dao.ClienteDAO;
+import com.elsantigestion.dao.ServicioEventualDAO;
+import com.elsantigestion.model.Cliente;
+import com.elsantigestion.model.ServicioEventual;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class ServicioEventualForm extends Stage {
+
+	@SuppressWarnings("unused")
+	private Runnable onSaveCallBack;
+	@SuppressWarnings("unused")
+	private ServicioEventualDAO dao;
+    private ClienteDAO clienteDao;
+    private double xOffset;
+    private double yOffset;
+    private Label titulo;
+    private Label lblFechaFinal;
+    private Label lblPrecio;
+    private Label lblGastos;
+    private Label lblMontoFinal;
+    private ComboBox<Cliente> cmbCliente;
+    private Button btnGuardar;
+    private Button btnCerrar;
+    private DatePicker dpFechaProgramada;
+    private TextField txtPrecio;
+    private TextField txtGastos;
+    private TextField txtMontoFinal;
+    private Region spacer;
+    private Region spacerFechaFinal;
+    private Region spacerPrecio;
+    private Region spacerGastos;
+    private Region spacerMontoFinal;
+    private GridPane grid;
+    private Scene scene;
+    private HBox barraSuperior;
+    private HBox boxPrecio;
+    private HBox boxGastos;
+    private HBox boxFechaFinal;
+    private HBox boxMontoFinal;
+
+    @SuppressWarnings("exports") 
+    public ServicioEventualForm(ServicioEventualDAO dao, Runnable onSaveCallBack, ServicioEventual servicioExistente) {
+
+        this.dao = dao;
+        this.onSaveCallBack = onSaveCallBack;
+        this.xOffset = 0;
+        this.yOffset = 0;
+        this.initStyle(StageStyle.UNDECORATED);
+
+        clienteDao = new ClienteDAO();
+        grid = new GridPane();
+        btnGuardar = new Button("Guardar");
+        btnCerrar = new Button("X");
+        spacer = new Region();
+        spacerFechaFinal = new Region();
+        spacerPrecio = new Region();
+        spacerMontoFinal = new Region();
+        spacerGastos = new Region();
+        lblFechaFinal = new Label("Fecha programada: ");
+        lblPrecio = new Label("Precio:");
+        lblGastos = new Label("Costos/insumos:");
+        lblMontoFinal = new Label("Monto a cobrar:");
+        dpFechaProgramada = new DatePicker();
+        barraSuperior = new HBox();
+        boxFechaFinal = new HBox();
+        boxPrecio = new HBox();
+        boxGastos = new HBox();
+        boxMontoFinal = new HBox();
+        txtPrecio = new TextField();
+        txtGastos = new TextField();
+        txtMontoFinal = new TextField();
+        scene = new Scene(grid, 400, 550);
+        
+        List<Cliente> clientes = clienteDao.obtenerClientes();
+        cmbCliente = new ComboBox<>();
+        cmbCliente.getItems().addAll(clientes);
+        cmbCliente.setPromptText("Seleccione un cliente...");
+
+        // Mostrar nombre pero mantener ID internamente
+        cmbCliente.setCellFactory(listView -> new javafx.scene.control.ListCell<Cliente>() {
+            @Override
+            protected void updateItem(Cliente item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setText(null);
+                } else {
+                    setText(item.getId() + " - " + item.getNombre());
+                }
+            }
+        });
+        cmbCliente.setButtonCell(new javafx.scene.control.ListCell<Cliente>() {
+            @Override
+            protected void updateItem(Cliente item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setText(null);
+                } else {
+                    setText(item.getId() + " - " + item.getNombre());
+                }
+            }
+        });
+
+        txtPrecio.setPromptText("Ingrese precio...");
+        txtGastos.setPromptText("Ingrese gastos...");
+        
+        txtPrecio.setMaxWidth(150);
+        txtGastos.setMaxWidth(150);
+        txtMontoFinal.setMaxWidth(150);
+        
+        txtMontoFinal.setEditable(false);
+        txtMontoFinal.setDisable(true);
+        
+        txtPrecio.textProperty().addListener((obs, oldVal, newVal) -> actualizarMontoFinal());
+        txtGastos.textProperty().addListener((obs, oldVal, newVal) -> actualizarMontoFinal());
+        
+
+        if (servicioExistente != null) {
+            titulo = new Label("Modificar servicio");
+            // Seleccionamos el cliente correcto en el ComboBox
+            clientes.stream()
+                    .filter(c -> c.getId() == servicioExistente.getClienteId())
+                    .findFirst()
+                    .ifPresent(cmbCliente::setValue);
+
+            dpFechaProgramada.setValue(servicioExistente.getFechaProgramada());
+            txtPrecio.setText(String.valueOf(servicioExistente.getPrecio()));
+            txtGastos.setText(String.valueOf(servicioExistente.getGastos()));
+            txtMontoFinal.setText(String.valueOf(servicioExistente.getMontoFinal()));
+        } else {
+            titulo = new Label("Agregar servicio");
+        }
+        
+        boxFechaFinal.setAlignment(Pos.TOP_RIGHT);
+        boxFechaFinal.setSpacing(10);
+        boxFechaFinal.getChildren().addAll(lblFechaFinal, spacerFechaFinal, dpFechaProgramada);
+        
+        boxPrecio.setAlignment(Pos.TOP_RIGHT);
+        boxPrecio.setSpacing(10);
+        boxPrecio.getChildren().addAll(lblPrecio, spacerPrecio, txtPrecio);
+        
+        boxGastos.setAlignment(Pos.TOP_RIGHT);
+        boxGastos.setSpacing(10);
+        boxGastos.getChildren().addAll(lblGastos, spacerGastos, txtGastos);
+        
+        boxMontoFinal.setAlignment(Pos.TOP_RIGHT);
+        boxMontoFinal.setSpacing(10);
+        boxMontoFinal.getChildren().addAll(lblMontoFinal, spacerMontoFinal, txtMontoFinal);
+
+        barraSuperior.setAlignment(Pos.TOP_RIGHT);
+        barraSuperior.setSpacing(10);
+        barraSuperior.getChildren().addAll(titulo, spacer, btnCerrar);
+        barraSuperior.getStyleClass().add("topBar");
+        barraSuperior.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+        barraSuperior.setOnMouseDragged(event -> {
+            barraSuperior.getScene().getWindow().setX(event.getSceneX() - xOffset);
+            barraSuperior.getScene().getWindow().setY(event.getSceneY() - yOffset);
+        });
+        
+        HBox.setMargin(lblFechaFinal, new Insets(10, 10, 10, 10));
+        HBox.setHgrow(spacerFechaFinal, Priority.ALWAYS);
+        
+        HBox.setMargin(lblPrecio, new Insets(10, 10, 10, 10));
+        HBox.setHgrow(spacerPrecio, Priority.ALWAYS);
+        
+        HBox.setMargin(lblGastos, new Insets(10, 10, 10, 10));
+        HBox.setHgrow(spacerGastos, Priority.ALWAYS);
+        
+        HBox.setMargin(lblMontoFinal, new Insets(10, 10, 10, 10));
+        HBox.setHgrow(spacerMontoFinal, Priority.ALWAYS);
+
+        HBox.setMargin(titulo, new Insets(15, 10, 0, 10));
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+
+        btnGuardar.getStyleClass().add("boton");
+        btnGuardar.setOnAction(e -> {
+            
+        	Cliente clienteSeleccionado = cmbCliente.getValue();
+
+            if (servicioExistente == null) {
+                ServicioEventual nuevo = new ServicioEventual(
+                        0,
+                        clienteSeleccionado.getId(),
+                        LocalDate.now(),
+                        dpFechaProgramada.getValue(),
+                        Double.parseDouble(txtPrecio.getText()),
+                        Double.parseDouble(txtGastos.getText()),
+                        Double.parseDouble(txtMontoFinal.getText()));
+                dao.agregarServicioEventual(nuevo);
+            } else {
+            	servicioExistente.setClienteId(clienteSeleccionado.getId());
+                servicioExistente.setFechaProgramada(dpFechaProgramada.getValue());
+                servicioExistente.setPrecio(Double.parseDouble(txtPrecio.getText()));
+                servicioExistente.setGastos(Double.parseDouble(txtGastos.getText()));
+                servicioExistente.setMontoFinal(Double.parseDouble(txtMontoFinal.getText()));
+               
+                dao.actualizarServicioEventual(servicioExistente);
+
+            }
+
+            if (onSaveCallBack != null) {
+                onSaveCallBack.run();
+            }
+            close();
+        });
+
+        btnCerrar.setOnAction(e -> close());
+
+        // LAYOUT //////////////////////////////
+
+        grid.getStyleClass().add("formulario");
+        grid.setPadding(new Insets(10));
+        grid.add(barraSuperior, 0, 0);
+        grid.addRow(1, cmbCliente);
+        grid.addRow(2, boxFechaFinal);
+        grid.addRow(3, boxPrecio);
+        grid.addRow(4, boxGastos);
+        grid.addRow(5, boxMontoFinal);
+        grid.add(btnGuardar, 0, 6);
+        GridPane.setMargin(cmbCliente, new Insets(30, 20, 10, 20));
+        GridPane.setMargin(boxFechaFinal, new Insets(0, 20, 0, 20));
+        GridPane.setMargin(boxPrecio, new Insets(0, 20, 0, 20));
+        GridPane.setMargin(boxGastos, new Insets(0, 20, 0, 20));
+        GridPane.setMargin(boxMontoFinal, new Insets(0, 20, 25, 20));
+
+        setScene(scene);
+        initModality(Modality.APPLICATION_MODAL);
+
+        scene.getStylesheets().add(
+                getClass().getResource("/com/elsantigestion/css/formulario.css").toExternalForm());
+
+    }
+    
+    private void actualizarMontoFinal() {
+        try {
+            double precio = Double.parseDouble(txtPrecio.getText());
+            double gastos = Double.parseDouble(txtGastos.getText());
+            double montoFinal = precio - gastos;
+            txtMontoFinal.setText(String.valueOf(montoFinal));
+        } catch (NumberFormatException e) {
+        }
+    }
+}
