db161d6de6f685444a87a88395d675319b55a52e
DevepMau <oshiettecs@gmail.com>
2025-11-08 16:32:31 -0300

    Cambios en el module-info para evitar warnings innecesarios.

--- elSantiGestion/src/module-info.java
+++ elSantiGestion/src/module-info.java
@@ -5,12 +5,15 @@
  * 
  */
 module elSantiGestion {
+	requires transitive javafx.controls;
 	requires java.sql;
 	requires javafx.graphics;
-	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.base;
 	
 	exports com.elsantigestion;
 	exports com.elsantigestion.ui;
+	exports com.elsantigestion.model;
+    exports com.elsantigestion.dao;
+    exports com.elsantigestion.utils;
 }
