6b031e0b22248453711a770fcfffe47359b21d5d
DevepMau <oshiettecs@gmail.com>
2025-11-21 21:21:17 -0300

    Agrega botón de swap, pie de tabla y gestión de iconos en TrabajoView
    - Añade botón para alternar entre tablas activas e inactivas
    - Incorpora pie de tabla con título dinámico según el estado
    - Elimina la columna "Activo" y su lógica asociada
    - Agrega iconos y tooltips para swap y recuperar
    - Refactoriza métodos para alternar iconos y títulos
    - Mejora la estructura visual con barra de acciones y footer

--- elSantiGestion/src/com/elsantigestion/ui/TrabajoView.java
+++ elSantiGestion/src/com/elsantigestion/ui/TrabajoView.java
@@ -2,12 +2,11 @@ package com.elsantigestion.ui;
 
 import com.elsantigestion.model.Trabajo;
 
-import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.control.TableCell;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.Tooltip;
@@ -20,22 +19,33 @@ import javafx.scene.layout.VBox;
 public class TrabajoView extends VBox {
 	
 	private final int iconoTamaño = 50;
+	private final String TITULO_INACTIVOS = "Tabla de trabajos archivados e inactivos";
+	private final String TITULO_ACTIVOS = "Tabla de trabajos activos";
 	
 	private TableView<Trabajo> tabla;
 	private Button btnNuevo;
 	private Button btnModificar;
 	private Button btnEliminar;
+	private Button btnSwap;
 	
 	private Image agregar;
 	private Image modificar;
 	private Image eliminar;
+	private Image swap;
+	private Image recover;
 	private ImageView iconoAgregar;
 	private ImageView iconoModificar;
 	private ImageView iconoEliminar;
+	private ImageView iconoSwap;
+	private ImageView iconoRecover;
 	private Tooltip tooltipNuevo;
 	private Tooltip tooltipModificar;
 	private Tooltip tooltipEliminar;
+	private Tooltip tooltipSwap;
+	private Tooltip tooltipRecover;
 	private HBox barraAcciones;
+	private HBox footer;
+	private Label lblFooter;
 	
 	public TrabajoView() {
 		
@@ -43,17 +53,26 @@ public class TrabajoView extends VBox {
 		btnNuevo = new Button();
 		btnModificar = new Button();
 		btnEliminar = new Button();
+		btnSwap = new Button();
 		
 		agregar = new Image(getClass().getResource("/iconos/add.png").toExternalForm());
 		modificar = new Image(getClass().getResource("/iconos/edit.png").toExternalForm());
 		eliminar = new Image(getClass().getResource("/iconos/delete.png").toExternalForm());
+		swap = new Image(getClass().getResource("/iconos/table-swap.png").toExternalForm());
+		recover = new Image(getClass().getResource("/iconos/recover.png").toExternalForm());
 		iconoAgregar = new ImageView(agregar);
 		iconoModificar = new ImageView(modificar);
 		iconoEliminar = new ImageView(eliminar);
+		iconoSwap = new ImageView(swap);
+		iconoRecover = new ImageView(recover);
 		tooltipNuevo = new Tooltip("Agregar trabajo");
 		tooltipModificar = new Tooltip("Modificar trabajo");
 		tooltipEliminar = new Tooltip("Eliminar trabajo");
-		barraAcciones = new HBox(2, btnEliminar, btnModificar, btnNuevo);
+		tooltipSwap = new Tooltip("Cambiar tabla");
+		tooltipRecover = new Tooltip("Recuperar trabajo");
+		lblFooter = new Label(TITULO_ACTIVOS);
+		barraAcciones = new HBox(2, btnSwap, btnEliminar, btnModificar, btnNuevo);
+		footer = new HBox(lblFooter);
 		
 		iconoAgregar.setFitWidth(iconoTamaño);
 		iconoAgregar.setFitHeight(iconoTamaño);
@@ -61,71 +80,57 @@ public class TrabajoView extends VBox {
 		iconoModificar.setFitHeight(iconoTamaño);
 		iconoEliminar.setFitWidth(iconoTamaño);
 		iconoEliminar.setFitHeight(iconoTamaño);
+		iconoSwap.setFitWidth(iconoTamaño);
+		iconoSwap.setFitHeight(iconoTamaño);
+		iconoRecover.setFitHeight(iconoTamaño);
+		iconoRecover.setFitWidth(iconoTamaño);
 		
 		btnNuevo.getStyleClass().add("barra-boton");
 		btnModificar.getStyleClass().add("barra-boton");
 		btnEliminar.getStyleClass().add("barra-boton");
+		btnSwap.getStyleClass().add("barra-boton");
 		
 		btnNuevo.setGraphic(iconoAgregar);
 		btnModificar.setGraphic(iconoModificar);
 		btnEliminar.setGraphic(iconoEliminar);
+		btnSwap.setGraphic(iconoSwap);
 		
 		btnNuevo.setTooltip(tooltipNuevo);
 		btnModificar.setTooltip(tooltipModificar);
 		btnEliminar.setTooltip(tooltipEliminar);
+		btnSwap.setTooltip(tooltipSwap);
 		
 		barraAcciones.setAlignment(Pos.CENTER_RIGHT);
 		barraAcciones.getStyleClass().add("barra");
+		
+		footer.setAlignment(Pos.CENTER_RIGHT);
+		footer.getStyleClass().add("footer");
 		
 		TableColumn<Trabajo, String> colNombre = new TableColumn<>("Nombre");
 		TableColumn<Trabajo, String> colDetalle = new TableColumn<>("Detalle");
 		TableColumn<Trabajo, Number> colPrecio = new TableColumn<>("Precio");
 		TableColumn<Trabajo, String> colUnidad = new TableColumn<>("Unidad");
-		TableColumn<Trabajo, Boolean> colActivo = new TableColumn<>("Activo");
 		
 		colNombre.setCellValueFactory(c -> new SimpleStringProperty(c.getValue().getNombre()));
 		colDetalle.setCellValueFactory(c -> new SimpleStringProperty(c.getValue().getDetalle()));
 		colPrecio.setCellValueFactory(c -> new SimpleDoubleProperty(c.getValue().getPrecio()));
 		colUnidad.setCellValueFactory(c -> new SimpleStringProperty(c.getValue().getUnidad()));
-		colActivo.setCellValueFactory(c -> new ReadOnlyObjectWrapper<>(c.getValue().isActivo()));
 		
 		colNombre.getStyleClass().add("columna-texto");
 		colDetalle.getStyleClass().add("columna-texto");
 		colPrecio.getStyleClass().add("columna-numero");
 		colUnidad.getStyleClass().add("columna-especial");
-		colActivo.getStyleClass().add("columna-especial");
-		
-		colActivo.setCellFactory(col -> new TableCell<Trabajo, Boolean>() {
-			protected void updateItem(Boolean item, boolean empty) {
-				super.updateItem(item, empty);
-				if(empty || item == null) {
-					setText(null);
-					setStyle("");	
-				} 
-				else {
-					if(item) {	
-						setText("Activo");
-						setStyle("-fx-text-fill: green; -fx-padding: 0 0 -1 0;");	
-					} else {
-						setText("Inactivo");
-						setStyle("-fx-text-fill: red; -fx-padding: 0 0 -1 0;");
-					}
-				}
-				setAlignment(Pos.CENTER);
-			}
-		});
 		
 		tabla.getColumns().add(colNombre);
 		tabla.getColumns().add(colDetalle);
 		tabla.getColumns().add(colPrecio);
 		tabla.getColumns().add(colUnidad);
-		tabla.getColumns().add(colActivo);
     	tabla.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
     	tabla.setPrefWidth(Double.MAX_VALUE);
     	VBox.setVgrow(tabla, Priority.ALWAYS);
 		
 		
-		this.getChildren().addAll(barraAcciones, tabla);
+		this.getChildren().addAll(barraAcciones, tabla, footer);
 		this.getStylesheets().add(
     	        getClass().getResource("/com/elsantigestion/css/tabla.css").toExternalForm()
     	    );
@@ -134,17 +139,23 @@ public class TrabajoView extends VBox {
 		colDetalle.setMinWidth(280);
 		colPrecio.setMinWidth(80);
 		colUnidad.setMinWidth(80);
-		colActivo.setMinWidth(80);
 		
 	
 	}
 
-	public TableView<Trabajo> getTabla() {
-		return tabla;
-	}
-
-	public void setTabla(TableView<Trabajo> tabla) {
-		this.tabla = tabla;
+	public TableView<Trabajo> getTabla(){
+    	return tabla;
+    }
+	
+	public void alternarIcono(Boolean boo) {
+		if(boo) {
+			btnEliminar.setGraphic(iconoEliminar);
+			btnEliminar.setTooltip(tooltipEliminar);
+		}
+		else {
+			btnEliminar.setGraphic(iconoRecover);
+			btnEliminar.setTooltip(tooltipRecover);
+		}
 	}
 
 	public Button getBtnNuevo() {
@@ -169,6 +180,31 @@ public class TrabajoView extends VBox {
 
 	public void setBtnEliminar(Button btnEliminar) {
 		this.btnEliminar = btnEliminar;
+	}
+	
+	public Button getBtnSwap() {
+		return btnSwap;
+	}
+	
+	public void setBtnSwap(Button btnSwap) {
+		this.btnSwap = btnSwap;
+	}
+
+	public String getTITULO_INACTIVOS() {
+		return TITULO_INACTIVOS;
+	}
+
+	public String getTITULO_ACTIVOS() {
+		return TITULO_ACTIVOS;
+	}
+	
+	public void setTituloDeTabla(Boolean boo) {
+		if(boo) {
+			this.lblFooter.setText(TITULO_ACTIVOS);
+		}
+		else {
+			this.lblFooter.setText(TITULO_INACTIVOS);
+		}
 	}
 
 }
