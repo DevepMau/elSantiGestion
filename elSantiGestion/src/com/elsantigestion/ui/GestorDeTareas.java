315c9f7158c5aeed7a65b47c7ab117cac8fe7c2e
DevepMau <oshiettecs@gmail.com>
2025-12-05 01:00:22 -0300

    Mejora la UI de GestorDeTareas y agrega estilos CSS personalizados
    - Cambia la herencia de HBox a VBox en GestorDeTareas
    - Añade título y estructura visual con HBox y GridPane
    - Aplica la hoja de estilos gestorDeTareas.css a la vista
    - Crea gestorDeTareas.css con estilos para tabla y título
    - Agrega métodos getter/setter para el label de título
    - Actualiza el archivo de base de datos gestion.db

--- elSantiGestion/src/com/elsantigestion/ui/GestorDeTareas.java
+++ elSantiGestion/src/com/elsantigestion/ui/GestorDeTareas.java
@@ -8,26 +8,35 @@ import com.elsantigestion.model.ServicioTrabajo;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
-public class GestorDeTareas extends HBox {
+public class GestorDeTareas extends VBox {
+	
+	private final String TITULO = "Lista de tareas";
 	
 	private Servicio servicioSeleccionado;
 	private TableView<ServicioTrabajo> tabla;
 	private HashMap<Integer, String> listaClientes;
 	private HashMap<Integer, String> listaTrabajos;
-	private HBox boxTabla;
-	private HBox boxOpciones;
+	private GridPane grid;
+	private Label lblTitulo;
+	private HBox boxTitulo;
+	private HBox boxContenido;
 	
 	public GestorDeTareas(HashMap<Integer, String> listaClientes, HashMap<Integer, String> listaTrabajos) {
 		
 		this.listaClientes = listaClientes;
 		this.listaTrabajos = listaTrabajos;
 		this.tabla = new TableView<>();
-		this.boxTabla = new HBox(tabla);
-		this.boxOpciones = new HBox();
+		this.lblTitulo = new Label(TITULO);
+		this.boxTitulo = new HBox(lblTitulo);
+		this.grid = new GridPane();
+		this.boxContenido = new HBox(tabla, grid);
 		
 		TableColumn<ServicioTrabajo, String> colTrabajo = new TableColumn<>("Trabajo");
 		TableColumn<ServicioTrabajo, Integer> colCantidad = new TableColumn<>("Cantidad");
@@ -45,6 +54,10 @@ public class GestorDeTareas extends HBox {
 		colCantidad.getStyleClass().add("columna-personalizada");
 		colEstado.getStyleClass().add("columna-personalizada");
 		
+		this.getStyleClass().add("box-tabla");
+		boxContenido.getStyleClass().add("box-tabla");
+		boxTitulo.getStyleClass().add("box-titulo");
+		
 		tabla.getColumns().add(colTrabajo);
 		tabla.getColumns().add(colCantidad);
 		tabla.getColumns().add(colEstado);
@@ -53,11 +66,11 @@ public class GestorDeTareas extends HBox {
 		tabla.setMaxWidth(Double.MAX_VALUE);
 		HBox.setHgrow(tabla, javafx.scene.layout.Priority.ALWAYS);
 		
-		this.getChildren().addAll(boxTabla, boxOpciones);
+		this.getChildren().addAll(boxTitulo, boxContenido);
 		
-		/*this.getStylesheets().add(
-    	        getClass().getResource("/com/elsantigestion/css/trabajoChecker.css").toExternalForm()
-    	   );*/
+		this.getStylesheets().add(
+    	        getClass().getResource("/com/elsantigestion/css/gestorDeTareas.css").toExternalForm()
+    	   );
 		
 	}
 	
@@ -87,6 +100,14 @@ public class GestorDeTareas extends HBox {
 
 	public void setListaTrabajos(HashMap<Integer, String> listaTrabajos) {
 		this.listaTrabajos = listaTrabajos;
+	}
+
+	public Label getLblTitulo() {
+		return lblTitulo;
+	}
+
+	public void setLblTitulo(Label lblTitulo) {
+		this.lblTitulo = lblTitulo;
 	}
 
 }
