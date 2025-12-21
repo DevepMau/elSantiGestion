0e6ba7139d6898568b45263c510b5882687c5ad9
DevepMau <oshiettecs@gmail.com>
2025-12-21 15:17:02 -0300

    Actualiza KanbanTarjeta para usar Cliente y ServicioDetalles
    - Cambia el constructor para recibir Cliente y ServicioDetalles
    - Reemplaza lblTitulo y lblDescripcion por lblCliente, lblTrabajo y
    lblCantidad
    - Ajusta el fondo según el color del cliente
    - Modifica los métodos getTarea, setTarea, getTitulo y getDescripcion
    - Elimina referencias a TareaProvisorio

--- elSantiGestion/src/com/elsantigestion/ui/KanbanTarjeta.java
+++ elSantiGestion/src/com/elsantigestion/ui/KanbanTarjeta.java
@@ -1,6 +1,7 @@
 package com.elsantigestion.ui;
 
-import com.elsantigestion.model.TareaProvisorio;
+import com.elsantigestion.model.Cliente;
+import com.elsantigestion.model.ServicioDetalles;
 
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
@@ -8,18 +9,21 @@ import javafx.scene.layout.VBox;
 
 public class KanbanTarjeta extends Pane {
 	
-	private TareaProvisorio tarea;
-	private Label lblTitulo;
-	private Label lblDescripcion;
+	private ServicioDetalles tarea;
+	private Label lblCliente;
+	private Label lblTrabajo;
+	private Label lblCantidad;
 	private VBox contenedor;
 	
-	public KanbanTarjeta(TareaProvisorio tarea) {
+	public KanbanTarjeta(Cliente cliente, ServicioDetalles tarea) {
 		this.setTarea(tarea);
-		this.lblTitulo = new Label(this.tarea.getTitulo());
-		this.lblDescripcion = new Label(this.tarea.getDescripcion());
-		this.contenedor = new VBox(5, lblTitulo, lblDescripcion);
+		this.lblCliente = new Label(cliente.getNombre());
+		this.lblTrabajo = new Label(tarea.getTrabajo().getNombre());
+		this.lblCantidad = new Label("cant: "+tarea.getCantidad()+"/"+tarea.getCantidad());
+		this.contenedor = new VBox(5, lblCliente, lblTrabajo, lblCantidad);
 		
 		this.getStyleClass().add("mi-pane");
+		this.setStyle("-fx-background-color: " + cliente.getColor() + ";");
 		contenedor.getStyleClass().add("tarjeta");
 		
 		this.getChildren().addAll(contenedor);
@@ -31,20 +35,20 @@ public class KanbanTarjeta extends Pane {
 	
 	/////////////////////////////////////////////////////
 
-	public TareaProvisorio getTarea() {
+	public ServicioDetalles getTarea() {
 		return tarea;
 	}
 
-	public void setTarea(TareaProvisorio tarea) {
+	public void setTarea(ServicioDetalles tarea) {
 		this.tarea = tarea;
 	}
 	
 	public String getTitulo() {
-		return lblTitulo.getText();
+		return lblCliente.getText();
 	}
 	
 	public String getDescripcion() {
-		return lblDescripcion.getText();
+		return lblTrabajo.getText();
 	}
 	
 	
