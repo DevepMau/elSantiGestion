24147a5fe98fddd886a6cd03bbd640ad710a8b78
DevepMau <oshiettecs@gmail.com>
2025-12-21 21:07:41 -0300

    Refactoriza KanbanController para soportar DAOs y gestión de tareas
    - Añade dependencias a ClienteDAO y ServicioDetallesDAO
    - Implementa carga y asignación de tareas a columnas según estado
    - Mejora el soporte de drag and drop con actualización de estado
    - Permite actualizar la lista de tareas desde el exterior
    - Añade métodos getter y setter para listaTareas
    - Refactoriza inicialización y manejo de tarjetas Kanban

--- elSantiGestion/src/com/elsantigestion/controller/KanbanController.java
+++ elSantiGestion/src/com/elsantigestion/controller/KanbanController.java
@@ -1,30 +1,81 @@
 package com.elsantigestion.controller;
 
+import java.util.List;
+
+import com.elsantigestion.dao.ClienteDAO;
+import com.elsantigestion.dao.ServicioDetallesDAO;
+import com.elsantigestion.model.Cliente;
+import com.elsantigestion.model.ServicioDetalles;
 import com.elsantigestion.ui.KanbanColumna;
 import com.elsantigestion.ui.KanbanView;
 import com.elsantigestion.ui.KanbanTarjeta;
 
+import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.VBox;
 
 public class KanbanController {
 	
-	private KanbanView tablero;
+	private final String POR_HACER = "Para Hacer";
+	private final String EN_PROGRESO = "En Progreso";
+	private final String COMPLETADO = "Completado";
+	private final String ARCHIVADO = "Archivado";
 	
-	public KanbanController(KanbanView tablero) {
+	private KanbanView tablero;
+	private List<ServicioDetalles> listaTareas;
+	private ClienteDAO clienteDao;
+	private ServicioDetallesDAO detallesDao;
+	
+	public KanbanController(KanbanView tablero, ServicioDetallesDAO sdDao, ClienteDAO cDao) {
 		this.tablero = tablero;
+		this.clienteDao = cDao;
+		this.detallesDao = sdDao;
+		this.listaTareas = detallesDao.obtenerTodos();
 		inicializar();
 	}
 	
 	private void inicializar() {
-		setOnDragAndDrop(tablero.getColPorHacer());
-		setOnDragAndDrop(tablero.getColEnProgreso());
-		setOnDragAndDrop(tablero.getColCompletado());
-		setOnDragAndDrop(tablero.getColArchivado());
+		
+		for(ServicioDetalles tarea : listaTareas) {
+			Cliente cliente = clienteDao.obtenerClientePorId(tarea.getServicio().getClienteId());
+			KanbanTarjeta card = new KanbanTarjeta(cliente, tarea);
+			tablero.addCard(card);
+		}
+		
+		for(KanbanTarjeta card : tablero.getLista()) {
+			card.setOnDragDetected(event -> {
+	            Dragboard db = card.startDragAndDrop(TransferMode.MOVE);
+
+	            ClipboardContent content = new ClipboardContent();
+	            content.putString(card.getTitulo()+card.getDescripcion());
+	            System.out.println(content);
+	            db.setContent(content);
+	            
+	            db.setDragView(card.snapshot(null, null));
+
+	            event.consume();
+	        });
+			añadirTarjeta(card);
+		}
+		
+		setOnDragAndDrop(tablero.getColPorHacer(), POR_HACER);
+		setOnDragAndDrop(tablero.getColEnProgreso(), EN_PROGRESO);
+		setOnDragAndDrop(tablero.getColCompletado(), COMPLETADO);
+		setOnDragAndDrop(tablero.getColArchivado(), ARCHIVADO);
 	}
 	
-	private void setOnDragAndDrop(KanbanColumna col) {
+	private void añadirTarjeta(KanbanTarjeta card) {
+		String estado = card.getTarea().getEstado();
+		switch(estado) {
+		case POR_HACER -> tablero.getColPorHacer().añadirTarjeta(card);
+		case EN_PROGRESO -> tablero.getColEnProgreso().añadirTarjeta(card);
+		case COMPLETADO -> tablero.getColCompletado().añadirTarjeta(card);
+		default -> tablero.getColArchivado().añadirTarjeta(card);
+		}
+	}
+	
+	private void setOnDragAndDrop(KanbanColumna col, String nuevoEstado) {
 		col.setOnDragOver(e -> {
             if (e.getGestureSource() instanceof KanbanTarjeta && e.getDragboard().hasString()) {
                 e.acceptTransferModes(TransferMode.MOVE);
@@ -38,14 +89,25 @@ public class KanbanController {
             	KanbanTarjeta cardOrigen = (KanbanTarjeta) e.getGestureSource();
                 ((VBox) cardOrigen.getParent()).getChildren().remove(cardOrigen);
                 col.añadirTarjeta(cardOrigen);
+                cardOrigen.getTarea().setEstado(nuevoEstado);
+                detallesDao.actualizarEstado(cardOrigen.getTarea());
                 e.setDropCompleted(true);
             }
             e.consume();
         });
 	}
 	
-	public KanbanView getView() {
+	public KanbanView getView(List<ServicioDetalles> listaTareas) {
+		this.setListaTareas(listaTareas);
 		return this.tablero;
+	}
+
+	public List<ServicioDetalles> getListaTareas() {
+		return listaTareas;
+	}
+
+	public void setListaTareas(List<ServicioDetalles> listaTareas) {
+		this.listaTareas = listaTareas;
 	}
 
 }
